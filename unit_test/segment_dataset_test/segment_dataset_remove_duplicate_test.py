beb89e35389424ec2a4c33a4dc9bd8c79ce940dd
SulthanRafif <rsulthan5@gmail.com>
2026-04-25 18:59:19 +0700

    fix remove duplicate test

--- unit_test/segment_dataset_test/segment_dataset_remove_duplicate_test.py
+++ unit_test/segment_dataset_test/segment_dataset_remove_duplicate_test.py
@@ -1,10 +1,16 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+
+sys.path.insert(0, BASE_DIR)
 
 from segment_dataset import md5_hash, remove_duplicates
 
-paths = ["ALL_IDB Dataset/L1/Im103_0.jpg", "ALL_IDB Dataset/L1/Im103_0_copy.jpg"]
+paths = [
+    os.path.join(BASE_DIR, "ALL_IDB Dataset", "L1", "Im103_0.jpg"),
+    os.path.join(BASE_DIR, "ALL_IDB Dataset", "L1", "Im103_0_copy.jpg"),
+]
+
 result = remove_duplicates(paths)
 print("Hasil remove_duplicates:", result)
